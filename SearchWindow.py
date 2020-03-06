88f111961217b54699f3fda84507ef2ff61bd67c
MathiasM10 <mathias.moetz@gmail.com>
2020-03-06 17:06:47 +0100

    Test

---
+++ SearchWindow.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+def Return_Event(event):
+    SearchWindow.destroy()
+
+
+SearchWindow = Tk()
+Txt1=Entry(master=SearchWindow, bg='white')
+#TODO Set Window pos to Top Left
+# SearchWindow.geometry('0+0')
+Txt1.pack()
+SearchWindow.bind_all('<Return>', Return_Event)
+Txt1.focus_set()
+SearchWindow.mainloop()
